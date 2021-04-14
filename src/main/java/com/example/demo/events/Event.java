0fdcf49b4cecb89b0adcd68a90f2c416501de198
hyuk <pjh08190819@naver.com>
2021-04-14 23:53:23 +0900

    이벤트 테스트

---
+++ src/main/java/com/example/demo/events/Event.java
@@ -0,0 +1,23 @@
+package com.example.demo.events;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public class Event {
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
---
+++ src/main/java/com/example/demo/events/EventStatus.java
@@ -0,0 +1,5 @@
+package com.example.demo.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGIN_ENROLLE;
+}
