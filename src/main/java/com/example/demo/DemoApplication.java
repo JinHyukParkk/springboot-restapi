13da548a5410f66672b03d4e0701abf96e7aca37
hyuk <pjh08190819@naver.com>
2021-05-02 23:30:22 +0900

    move bean file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,9 +11,4 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
