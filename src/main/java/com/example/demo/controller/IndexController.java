d9520a035bab8fd9dc3e192569242c2f7fc314fc
hyuk <pjh08190819@naver.com>
2021-06-02 23:50:03 +0900

    add index api

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public ResourceSupport index() {
-
+    @GetMapping("/api")
+    public RepresentationModel index() {
+        var index = new RepresentationModel<>();
+        index.add(linkTo(EventController.class).withRel("event"));
+        return index;
     }
 }
